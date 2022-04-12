f5abd23f4f325c0c556f19212601304ef3b0db72
Sara Ibarra <saraiba33@gmail.com>
2022-04-12 15:25:19 -0600

    Create home page and add style

--- index.js
+++ index.js
@@ -1,35 +1,66 @@
 const url = `https://fedeperin-harry-potter-api-en.herokuapp.com/db`
-const characterUrl = `http://hp-api.herokuapp.com/api/characters `
+const body = document.querySelector("body")
+
+// function addHeader() {
+
+// }
+
+// addHeader()
+
+function addMainContent() {
+    const header = document.createElement("header")
+    header.classList.add("main-header")
+    const headerContent = `
+    <h1>Welcome to The Wizarding World</h1>
+    `
+    header.innerHTML = headerContent
+    body.append(header)
+    const div = document.createElement("div")
+    div.classList.add("catagories")
+    const divContent = `
+    <a class="covers" href="characters.html"><img class="cover one" src="images/character-cover2.jpg" />
+    </a>
+    <a class="covers" href="houses.html"><img class="cover two" src="images/house-cover2.jpg" /></a>
+    <a class="covers" href="spells.html"><img class="cover three" src="images/spells-cover2.jpg" /></a>
+    <a class="covers" href="quiz.html"><img class="cover four" src="images/quiz-cover2.jpg" /></a>
+    `
+    div.innerHTML = divContent
+    body.append(div)
+}
+addMainContent()
 
 
-const getBooks = () => {
-    const promises = [];
-    for (let i = 1; i <= 8; i++) {
-        const url = `https://fedeperin-harry-potter-api-en.herokuapp.com/books/${i}`;
-        promises.push(fetch(url).then((response) => response.json()));
-    }
 
-    Promise.all(promises).then(results => {
-        return results.map(books => {
-            books.title
-        })
+
+fetch(url)
+    .then(response => {
+        return response.json()
+    }).then(parsedResponse => {
 
     })
-}
 
-// fetch(url)
-//     .then(response => {
-//         return response.json()
-//     }).then(parsedResponse => {
-//         const bookList = parsedResponse.books.map(book => )
-//         console.log(bookList)
-//     })
 
-function getCharacters() {
-    fetch(`http://hp-api.herokuapp.com/api/characters`)
-        .then(response => {
-            return response.json()
-        }).then(parsedResponse => {
-            console.log(parsedResponse)
-        })
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getCharacters() {
+//     fetch(`http://hp-api.herokuapp.com/api/characters`)
+//         .then(response => {
+//             return response.json()
+//         }).then(parsedResponse => {
+//             console.log(parsedResponse)
+//         })
+// }
