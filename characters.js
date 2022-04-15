589e77fd78f33b1a02decafddd43935e8386e602
Sara Ibarra <saraiba33@gmail.com>
2022-04-15 14:47:40 -0600

    add js fetch and house images

--- characters.js
+++ characters.js
@@ -36,7 +36,7 @@ getCharacters()
 const header = document.createElement("header")
 header.classList.add("main-header")
 const headerContent = `
-    <h1>Welcome to The Wizarding World</h1>
+    <h1>Characters</h1>
     `
 header.innerHTML = headerContent
 body.append(header)
--- houses.js
+++ houses.js
@@ -6,3 +6,49 @@ const headerContent = `
     `
 header.innerHTML = headerContent
 body.append(header)
+
+function getGryffindor() {
+    fetch(`http://hp-api.herokuapp.com/api/characters`)
+        .then(response => response.json())
+        .then(parsedResponse => {
+            const getFounder = parsedResponse.find(founder => founder.name === 'Godric Gryffindor')
+            console.log(getFounder)
+        })
+}
+
+getGodric()
+
+function getRavenClaw() {
+    fetch(`http://hp-api.herokuapp.com/api/characters`)
+        .then(response => response.json())
+        .then(parsedResponse => {
+            const getFounder = parsedResponse.find(founder => founder.name === 'Rowena Ravenclaw')
+            console.log(getFounder)
+        })
+}
+
+getRavenClaw()
+
+
+function getHufflepuff() {
+    fetch(`http://hp-api.herokuapp.com/api/characters`)
+        .then(response => response.json())
+        .then(parsedResponse => {
+            const getFounder = parsedResponse.find(founder => founder.name === 'Helga Hufflepuff')
+            console.log(getFounder)
+        })
+}
+
+getHufflepuff()
+
+
+function getGodric() {
+    fetch(`http://hp-api.herokuapp.com/api/characters`)
+        .then(response => response.json())
+        .then(parsedResponse => {
+            const getFounder = parsedResponse.find(founder => founder.name === 'Salazar Slytherin')
+            console.log(getFounder)
+        })
+}
+
+getGodric()
