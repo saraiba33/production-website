886f5e675234fa38eb7faacd6d37c1ec662c98f9
Sara Ibarra <saraiba33@gmail.com>
2022-04-19 20:23:06 -0600

    add quiz form and add 404 page

--- quiz.js
+++ quiz.js
@@ -2,6 +2,7 @@ const body = document.querySelector("body")
 const url = `https://fedeperin-harry-potter-api-en.herokuapp.com/db`
 const selectOne = document.querySelector(".character-select")
 const selectTwo = document.querySelector(".spell-select")
+const form = document.querySelector("form");
 
 
 
@@ -11,7 +12,7 @@ function characterOption() {
         .then(parsedResponse => {
             const getCharacters = parsedResponse.characters
             const onlyNames = getCharacters.map(characters => characters.character).filter((name, index) => [1, 12, 21].includes(index)).map(name => {
-                const option1 = document.createElement("option")
+                const option = document.createElement("option")
                 option.classList.add("character-options")
                 option.textContent = name
                 option.value = name
@@ -51,3 +52,29 @@ function spellOption() {
         })
 }
 spellOption()
+
+function getInputs() {
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const name = formData.get("name")
+        const selection1 = formData.get("character-select");
+        const selection2 = formData.get("spell-select");
+
+        console.log(selection1, selection2, name)
+            // if (selection1 === "Luna Lovegood" && selection2 === "Expecto Patronum") {
+            //     console.log(`${name} here is your Patronus`)
+            // } else if (selection1 === "Hermione Jean Granger" && selection2 === "Expulso") {
+            //     console.log(`${name} I can't find it`)
+            // }
+            // } else if (selection1 === "Hermione Jean Granger" && selection2 === "Exp") {
+
+        // } else if (selection1 === "Hermione Jean Granger" && selection2 === "Expecto Patronum") {
+
+        // } else if (selection1 === "Hermione Jean Granger" && selection2 === "Expecto Patronum") {
+
+        // } else(selection1 === "Hermione Jean Granger" && selection2 === "Expecto Patronum") {
+
+        // }
+    })
+}
